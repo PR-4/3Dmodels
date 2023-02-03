c8e48681feb46620e8857f2ad3ae99c2e5b298bd
Victor Resproj <victorcarreira@id.uff.br>
2023-02-03 19:14:41 -0300

    Adiciona os debugers

---
+++ modules/dio.py
@@ -0,0 +1,29 @@
+#-*- coding: utf-8 -*-
+
+####### PACOTES INTERNOS ###########
+import os
+import sys
+import numpy as np
+import pandas as pd
+####################################
+
+
+class debug:
+    pass
+
+def pause():
+    '''
+    FORTRANIC logical debugging. 
+    Just for fortranic beings.
+    '''
+    programPause = input("Press the <ENTER> key to continue...")
+    return
+
+def stop():
+    '''
+    FORTRANIC logical debugging. 
+    Just for fortranic beings.
+    '''
+    sys.exit('Stop here!')
+    return
+
