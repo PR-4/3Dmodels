47f6d7b4fa5192a53e6107b0822e192df0241da1
Bernardo Chede <bechede@id.uff.br>
2024-01-18 20:07:02 -0300

    Testes e crição do modelo do SEAL

---
+++ input/SEAL/V1/Gempy_2.3.1/03_Generating_OP.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+
+
+path_processed = "../Gempy_2.3.1/surface_points_less_crono_scaled.csv"
+
+# Carregue seus dados
+surface_points_df = pd.read_csv(path_processed)
+# orientation_points_df = pd.read_csv(path_processed + "orientations_points_example.csv")
+
+# Filtrar pontos de superfície para a formação 'topo'
+topo_surface_points = surface_points_df[surface_points_df["formation"] == "EOC-SUP"]
+
+# Crie um novo DataFrame para armazenar os pontos de orientação
+# new_orientation_points = pd.DataFrame(columns=orientation_points_df.columns)
+
+# Obtenha os limites e o centro do grid
+xmin, xmax = topo_surface_points["X"].min(), topo_surface_points["X"].max()
+ymin, ymax = topo_surface_points["Y"].min(), topo_surface_points["Y"].max()
+xcenter, ycenter = (xmin + xmax) / 2, (ymin + ymax) / 2
+
+distances = np.sqrt((topo_surface_points["X"] - xcenter) ** 2 + (topo_surface_points["Y"] - ycenter) ** 2)
+closest_index = distances.idxmin()
+
+# Obtenha o valor z do ponto central
+zcenter = topo_surface_points.loc[closest_index, "Z"]
+zcenter -= 20  # Subtract 20 from z
+zmin_x = topo_surface_points[topo_surface_points["X"] == xmin]["Z"].values[0] - 20
+zmax_x = topo_surface_points[topo_surface_points["X"] == xmax]["Z"].values[0] - 20
+zmin_y = topo_surface_points[topo_surface_points["Y"] == ymin]["Z"].values[0] - 20
+zmax_y = topo_surface_points[topo_surface_points["Y"] == ymax]["Z"].values[0] - 20
+
+# Get the rows for the boundary points
+row_min_x_min_y = topo_surface_points[(topo_surface_points["X"] == xmin) & (topo_surface_points["Y"] == ymin)].iloc[0]
+row_min_x_max_y = topo_surface_points[(topo_surface_points["X"] == xmin) & (topo_surface_points["Y"] == ymax)].iloc[0]
+row_max_x_min_y = topo_surface_points[(topo_surface_points["X"] == xmax) & (topo_surface_points["Y"] == ymin)].iloc[0]
+row_max_x_max_y = topo_surface_points[(topo_surface_points["X"] == xmax) & (topo_surface_points["Y"] == ymax)].iloc[0]
+row_center = topo_surface_points.loc[closest_index]
+
+# Create the 5 orientation points
+new_points = pd.DataFrame(
+    {
+        "X": [row_center["X"], row_min_x_min_y["X"], row_min_x_max_y["X"], row_max_x_min_y["X"], row_max_x_max_y["X"]],
+        "Y": [row_center["Y"], row_min_x_min_y["Y"], row_min_x_max_y["Y"], row_max_x_min_y["Y"], row_max_x_max_y["Y"]],
+        "Z": [
+            row_center["Z"] - 20,
+            row_min_x_min_y["Z"] - 20,
+            row_min_x_max_y["Z"] - 20,
+            row_max_x_min_y["Z"] - 20,
+            row_max_x_max_y["Z"] - 20,
+        ],
+        "azimuth": [0, 0, 0, 0, 0],
+        "dip": [0, 0, 0, 0, 0],
+        "polarity": [1, 1, 1, 1, 1],
+        "formation": ["topo", "topo", "topo", "topo", "topo"],
+    }
+)
+
+if new_orientation_points.empty:
+    new_orientation_points = new_points
+else:
+    new_orientation_points = pd.concat([new_orientation_points, new_points])
+
+# Salve o novo DataFrame para um arquivo CSV
+new_orientation_points.to_csv(path_processed + "generated_orientations_points.csv", index=False)
---
+++ input/SEAL/V1/Gempy_3.0/script.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+# read .txt as lines
+with open("../Gempy_3.0/surface_points.txt") as f:
+    lines = f.readlines()
+
+# remove the newline character at the end of each line and split each line by comma
+data = [line.rstrip().split(",") for line in lines]
+
+data = [[item for item in line.rstrip().split(",") if item] for line in lines]
+
+# create a DataFrame from the data
+df = pd.DataFrame(data[1:], columns=data[0])
+
+# convert X and Y to float
+df["X"] = df["X"].astype(float)
+df["Y"] = df["Y"].astype(float)
+
+# convert Z to int
+df["Z"] = df["Z"].astype(float).astype(int)
+
+df.info()
+df.describe()
+df.to_csv("../Gempy_3.0/surface_points_full_crono.csv", index=False)
+
+
+scaler_x = MinMaxScaler(feature_range=(0, df["X"].max() - df["X"].min()))
+scaler_y = MinMaxScaler(feature_range=(0, df["Y"].max() - df["Y"].min()))
+
+df[["X"]] = scaler_x.fit_transform(df[["X"]])
+df[["Y"]] = scaler_y.fit_transform(df[["Y"]])
+
+df["X"] = df["X"].round(1)
+df["Y"] = df["Y"].round(1)
+
+df["X"].min()
+df["X"].max()
+df["Y"].min()
+df["Y"].max()
+df["Z"].min()
+df["Z"].max()
+
+# Salvar em csv
+df.to_csv("../Gempy_3.0/surface_points_full_crono_scaled.csv", index=False)
+
+
+# print the DataFrame
+print(df)
